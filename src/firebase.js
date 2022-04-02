7c90f13f8efd36c66165b05320f6db7176290d20
shanegracias <shanegracias7@gmail.com>
2022-04-02 11:28:34 +0530

    fixed importing fire base issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
-import firebase from 'firebase/compat/app';
-import "firebase/compat/auth";
+import firebase from 'firebase/compat';
+import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBCtLqFCjwDHY7FloF_kDG1WrDuKgknyCo",
-    authDomain: "react-slack-clone-84e1d.firebaseapp.com",
-    projectId: "react-slack-clone-84e1d",
-    storageBucket: "react-slack-clone-84e1d.appspot.com",
-    messagingSenderId: "1086374017800",
-    appId: "1:1086374017800:web:362f7844c425dc61150abd",
-    measurementId: "G-Z97W6XC6BJ"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBCtLqFCjwDHY7FloF_kDG1WrDuKgknyCo",
+  authDomain: "react-slack-clone-84e1d.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-84e1d-default-rtdb.firebaseio.com",
+  projectId: "react-slack-clone-84e1d",
+  storageBucket: "react-slack-clone-84e1d.appspot.com",
+  messagingSenderId: "1086374017800",
+  appId: "1:1086374017800:web:362f7844c425dc61150abd",
+  measurementId: "G-Z97W6XC6BJ"
+};
+firebase.initializeApp(firebaseConfig);
 
-  export default firebase;
+
+
+export default firebase;
+
+
