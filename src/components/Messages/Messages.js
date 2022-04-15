2fb6f491a3d70ad1fd25fbfc60f8bb8a54583a4e
shanegracias <shanegracias7@gmail.com>
2022-04-15 10:01:12 +0530

    displaying messages in the chat

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,21 +4,58 @@ import firebase from "../../firebase";
 
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
 
 class Messages extends React.Component {
     state={
         messagesRef:firebase.database().ref('messages'),
         channel:this.props.currentChannel,
-        user:this.props.currentUser
+        user:this.props.currentUser,
+        messages:[],
+        messagesLoading:true
     }
+    componentDidMount(){
+        const {channel,user} = this.state;
+
+        if(channel&&user){
+            this.addListners(channel.id)
+        }
+    }
+    addListners = channelId =>{
+        this.addMessageListner(channelId)
+    }
+    addMessageListner = channelId =>{
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on('child_added',snap => {
+            loadedMessages.push(snap.val());
+            this.setState({
+                messages: loadedMessages,
+                messagesLoading: false
+            })
+        })
+    }
+    displayMessages = messages=>{
+       return(
+        messages.length>0 && messages.map(message =>(
+            <Message 
+                key={message.timestamp}
+                message={message}
+                user={this.state.user}
+            />))
+       ) 
+    }
+        
+    
     render() {
-        const { messagesRef,channel,user} = this.state
+        const { messagesRef,channel,user, messages} = this.state
         return (
         <React.Fragment>
             <MessagesHeader />
 
             <Segment>
-            <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+            <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+            </Comment.Group>
             </Segment>
 
             <MessageForm 
