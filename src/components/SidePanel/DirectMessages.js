40759a2784e6ab0f0973b3cdd4b096b4f73c7acd
shanegracias <shanegracias7@gmail.com>
2022-04-28 23:56:46 +0530

    added direct messages component

---
+++ src/components/SidePanel/DirectMessages.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Menu,Icon } from 'semantic-ui-react';
+
+export default class DirectMessages extends Component {
+    state ={
+        users:[]
+    }
+    render() {
+        const {users} = this.state;
+
+        return (
+        <Menu.Menu className='menu'>
+            <Menu.Item>
+                <span>
+                    <Icon name='mail'/> DIRECT MESSAGES
+                </span>{' '}
+                ({users.length})
+            </Menu.Item>
+            
+        </Menu.Menu>
+        )
+    }
+}
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Menu} from 'semantic-ui-react';
 import UserPanel from "./UserPanel";
 import Channels from "./Channels";
+import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component{
 
@@ -18,7 +19,8 @@ class SidePanel extends React.Component{
                 style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
             >
             <UserPanel currentUser={currentUser}/>
-            <Channels currentUser={currentUser}/>        
+            <Channels currentUser={currentUser}/>
+            <DirectMessages/>        
             </Menu>
         )
     }
