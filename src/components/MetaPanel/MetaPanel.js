3e2382385d54bc3b7cd5d04b4059dd367f1c3474
shanegracias <shanegracias7@gmail.com>
2022-06-01 09:23:40 +0530

    scaffold meta pannel component

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,9 +1,77 @@
 import React from "react";
+import {Segment,Header,Icon,Accordion} from 'semantic-ui-react';
 
 class MetaPanel extends React.Component{
+    state ={
+        activeIndex:0,
+        privateChannel:this.props.isPrivateChannel
+    }
+    
+    setActiveIndex =(event, titleProps)=>{
+        const {index} = titleProps
+        const {activeIndex} = this.state
+        
+        const newIndex = activeIndex === index ? -1:index;
+        this.setState({activeIndex:newIndex})
+    }
     render(){
+        const {activeIndex,privateChannel} = this.state
+        if(privateChannel) return null;
         return (
-            <div>MetaPanel</div>
+            <Segment>
+                <Header as={"h3"} attached="top">
+                    About # Channel 
+                </Header>
+                <Accordion styled attached="true">
+                    <Accordion.Title
+                        active={activeIndex === 0}
+                        index={0}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown"/>
+                        <Icon name="info"/>
+                        channel details
+                    </Accordion.Title>
+
+                    <Accordion.Content
+                        active={activeIndex === 0}
+                    >
+                        details
+                    </Accordion.Content>
+
+                    <Accordion.Title
+                        active={activeIndex === 1}
+                        index={1}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown"/>
+                        <Icon name="user circle"/>
+                        top posters
+                    </Accordion.Title>
+
+                    <Accordion.Content
+                        active={activeIndex === 1}
+                    >
+                        posters
+                    </Accordion.Content>
+
+                    <Accordion.Title
+                        active={activeIndex === 2}
+                        index={2}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown"/>
+                        <Icon name="pencil alternate"/>
+                        Created By
+                    </Accordion.Title>
+
+                    <Accordion.Content
+                        active={activeIndex === 2}
+                    >
+                        creators 
+                    </Accordion.Content>
+                </Accordion>
+            </Segment>
         )
     }
 }
